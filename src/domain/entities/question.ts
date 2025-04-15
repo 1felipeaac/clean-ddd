609c304b3fd0198cdbf42ed03f5b269c728599a8
Felipe Coelho <felipeaacoelho@gmail.com>
2025-04-15 14:23:32 -0300

    first commit

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,8 @@
+class Question {
+    public title: string
+    public content: string
+    constructor (title: string, content: string){
+        this.title = title,
+        this.content = content
+    }
+}
---
+++ src/domain/entities/student.ts
@@ -0,0 +1,10 @@
+class Students {
+    public name: string
+    public content: string
+
+    constructor (name: string, content: string){
+        this.name = name
+        this.content = content
+    }
+    
+}
